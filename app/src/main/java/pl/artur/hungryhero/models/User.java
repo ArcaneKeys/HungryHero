a2674afe63aeccd0ccd7cc8ec7ca8d0b3a14d2ea
Artur Kąkol0 <artur.kakol2000@gmail.com>
2023-07-09 11:55:55 +0200

    Dodanie property name do klasy user, #1

--- app/src/main/java/pl/artur/hungryhero/models/User.java
+++ app/src/main/java/pl/artur/hungryhero/models/User.java
@@ -1,8 +1,13 @@
 package pl.artur.hungryhero.models;
 
+import com.google.firebase.database.PropertyName;
+
 public class User {
+    @PropertyName("accountType")
     private String accountType;
+    @PropertyName("email")
     private String email;
+    @PropertyName("uid")
     private String uid;
 
     public User(String accountType, String email, String uid) {
